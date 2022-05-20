4d14034ab041d8973b85c8eb0a00d3da817c6d14
Ignacio Maidana <nachomaidana123@gmail.com>
2022-05-20 15:32:47 -0300

    Cosas que se me escaparon

---
+++ Practica clase 7/Entidades/Enumeradores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Enumeradores
+    {
+        public enum TamañoRam { dosGB, cuatroGB, ochoGB, dieciseisGB }
+    }
+}
